f7ac1f862f66bb7f931cc6bea61bc3449778d41d
Miralem.Ba <miralembanovi@gmail.com>
2024-04-11 15:40:15 +0200

    comment added

--- client/authentication/register.js
+++ client/authentication/register.js
@@ -1,30 +1,49 @@
+// Deaktiviert ESLint-Warnungen über nicht definierte Variablen.
 /* eslint-disable no-undef */
+
+// Wartet darauf, dass das gesamte DOM geladen ist, bevor JavaScript-Code ausgeführt wird.
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('loginNavigationButton').addEventListener('click', () => {
-      const login = '../index.html';
+  // Fügt einen Event-Listener für den 'Login' Navigationsbutton hinzu.  
+  document.getElementById('loginNavigationButton').addEventListener('click', () => {
+    // Definiert den Pfad zur Login-Seite.  
+    const login = '../index.html';
+    // Leitet den Benutzer zur Login-Seite um.
       window.location.href = `/${login}`;
     }); 
+
+    // Fügt einen Event-Listener für das Absenden des Registrierungsformulars hinzu.
     document.getElementById('registrationForm').addEventListener('submit', async (event) => {
+      // Verhindert das Standardverhalten des Formulars, welches die Seite neu laden würde.
       event.preventDefault();
+
+      // Holt die Benutzerdaten aus den Formularfeldern.
       const username = document.getElementById('username').value;
       const password = document.getElementById('password').value;
       const email = document.getElementById('email').value;
       try {
+
+        // Sendet eine POST-Anfrage an den Server, um den neuen Benutzer zu registrieren.
         const response = await fetch('http://localhost:4200/register', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
+
+          // Konvertiert die Benutzerdaten in einen JSON-String für den Request.
           body: JSON.stringify({ username, password, email }),
         });
+
+        // Überprüft, ob die Anfrage erfolgreich war.
         if (response.ok) {
-          console.log('Registrierung erfolgreich.');
+          console.log('Registrierung erfolgreich.'); // Gibt eine Erfolgsmeldung in der Konsole aus.
         } else {
+          // Gibt eine Fehlermeldung in der Konsole aus, falls die Anfrage fehlschlägt.
           console.error('Fehler bei der Registrierung:', response.statusText);
         }
       } catch (error) {
+
+        // Fängt Netzwerkfehler ab und gibt eine Fehlermeldung in der Konsole aus.
         console.error('Netzwerkfehler:', error);
       }
     });
-  });
-  
+  });  
--- client/comment/comment.js
+++ client/comment/comment.js
@@ -1,23 +1,32 @@
+// Parsen der URL-Parameter, um die postId zu erhalten
 const urlParams = new URLSearchParams(window.location.search);
 const postId = urlParams.get('postId');
+
+// Warten bis das DOM vollständig geladen ist
 document.addEventListener('DOMContentLoaded', () => {
+  // Referenz auf das Element, in dem Kommentare angezeigt werden
   const commentWindow = document.getElementById('commentWindow');
 
+  // Funktion zum Abrufen und Anzeigen von Kommentaren für den spezifischen Post
   const showTweets = async () => {
     try {
+      // Anfrage an den Server, um Kommentare abzurufen
       const respons = await fetch('http://localhost:4200/getComment', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
         },
       });
+      // Überprüfung, ob die Anfrage erfolgreich war
       if (respons.ok) {
         const result = await respons.json();
         const { allcomment } = result;
         console.log(allcomment);
 
+        // Durchlaufen aller Kommentare und Anzeigen, wenn sie zum aktuellen Post gehören
         for (let i = 0; i < allcomment.length; i++) {
           if (postId === `${allcomment[i].tweet_id}`) {
+            // Hinzufügen des Kommentars zum HTML
             commentWindow.innerHTML
             += `
             <div class="flex items-center mb-2 dark:bg-gray-800 p-2  p-4 rounded-lg shadow-md mb-4 w-1/2 mx-auto">
@@ -36,8 +45,11 @@ document.addEventListener('DOMContentLoaded', () => {
   };
   showTweets();
 
+  // Funktion zum Anzeigen des aktuellen Posts
   const showTweet = async () => {
     try {
+
+      // Anfrage an den Server, um den Post abzurufen
       const respons = await fetch('http://localhost:4200/getPost', {
         method: 'GET',
         headers: {
@@ -47,9 +59,13 @@ document.addEventListener('DOMContentLoaded', () => {
       if (respons.ok) {
         const result = await respons.json();
         const { allpost } = result;
+
+        // Durchlaufen aller Posts und Anzeigen des aktuellen Posts
         for (let i = 0; i < allpost.length; i++) {
           if (`${allpost[i].tweet_id}` === postId) {
             const postWindow = document.getElementById('postWindow');
+
+            // Hinzufügen des Posts zum HTML
             postWindow.innerHTML = `
                 <div id="Feedwindow" class="w-full bg-gradient-to-r rounded-lg ">
                     <div class="dark:bg-gray-800 p-4 rounded-lg shadow-md mb-4 w-1/2 mx-auto">
@@ -74,18 +90,21 @@ document.addEventListener('DOMContentLoaded', () => {
   
   showTweet();
 
+  // Event-Listener für den 'Kommentar posten'-Button
   document.getElementById('postCommentButton').addEventListener('click', async (event) => {
-    event.preventDefault();
-    console.log("cenifcwefo aefboa")
-    const commentMessage = document.getElementById('commentTextarea').value;
-    const jwtToken = localStorage.getItem('Token');
+    event.preventDefault(); // Verhindern des Seitenneuladens
+    console.log("cenifcwefo aefboa") 
+    const commentMessage = document.getElementById('commentTextarea').value; // Extrahieren der Kommentarnachricht
+    const jwtToken = localStorage.getItem('Token'); // JWT-Token aus dem Local Storage holen
+
+    // Versenden des Kommentars an den Server
     try {
       const response = await fetch('http://localhost:4200/createComment', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ commentMessage, jwtToken, postId }),
+        body: JSON.stringify({ commentMessage, jwtToken, postId }), // Kommentardaten zum Server senden
       });
       if (response.ok) {
         console.log('Kommentar erfolgreich gespeichert');
